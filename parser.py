fe8fe2572c7f275f88cafa2d08b7cae741b35019
Amaan Rajguru <amaan.rajguru@gmail.com>
2026-04-14 23:50:39 -0400

    Base Shell Setup

---
+++ parser.py
@@ -0,0 +1,17 @@
+def parse_input():
+    
+def syntax_checker():
+
+def process_token():
+    Keywords = {
+        "with": "WITH",
+        "select":"SELECT",
+        "from":"FROM",
+        "where":"WHERE",
+        "group by":"GROUP BY",
+        "such that":"SUCH THAT",
+        "having":"HAVING"
+    }
+    
+
+def write_output():
